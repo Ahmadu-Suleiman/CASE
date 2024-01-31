b03cab8d7c4849c10460898c90b7c15586c0435b
abdulhamid-abubakar <abdulhamidimam1226@gmail.com>
2024-01-31 15:24:53 +0100

    improved case page interface

--- lib/pages/case_page.dart
+++ lib/pages/case_page.dart
@@ -1,2 +1,15 @@
 import 'package:flutter/material.dart';
 
+class CasePage extends StatefulWidget {
+  const CasePage({super.key});
+
+  @override
+  State<CasePage> createState() => _CasePageState();
+}
+
+class _CasePageState extends State<CasePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
