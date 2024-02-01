a40ae92ed385f4c3349ad85eeaccd383e46dc571
abdulhamid-abubakar <abdulhamidimam1226@gmail.com>
2024-02-01 12:48:25 +0100

    little margin in the create case UI

--- lib/pages/create_case.dart
+++ lib/pages/create_case.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
+import 'package:http/http.dart' as http;
 
 class CreateCase extends StatefulWidget {
   const CreateCase({super.key});
@@ -14,154 +17,160 @@ class _CreateCaseState extends State<CreateCase> {
     'child3.jpg',
     'profile.png',
   ];
+  final ImagePicker _picker = ImagePicker();
 
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: ListView(
-        children: [
-          const Center(
-            child: Text(
-              'Create a new Case',
-              style: TextStyle(
-                fontSize: 35,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          const SizedBox(height: 20),
-          Center(
-            child: TextButton.icon(
-              onPressed: () {},
-              icon: const Icon(Icons.image),
-              label: const Text(
-                'Add main image',
-                style: TextStyle(
-                  fontSize: 14,
-                  color: Colors.blue,
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(height: 20),
-          const Center(
-            child: TextField(
-              decoration: InputDecoration(hintText: 'Case title'),
-              maxLines: 1,
-              style: TextStyle(
-                fontSize: 18,
-                color: Colors.black,
-              ),
-            ),
-          ),
-          const SizedBox(height: 20),
-          const Center(
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: 'Short description',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(8),
+      child: Container(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: ListView(
+            children: [
+              const Center(
+                child: Text(
+                  'Create a new Case',
+                  style: TextStyle(
+                    fontSize: 35,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
               ),
-              maxLines: 3,
-              style: TextStyle(
-                fontSize: 18,
-                color: Colors.black,
-              ),
-            ),
-          ),
-          const SizedBox(height: 20),
-          const TextField(
-            decoration: InputDecoration(
-              hintText: 'Detailed description',
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(8),
+              const SizedBox(height: 20),
+              Center(
+                child: TextButton.icon(
+                  onPressed: () {},
+                  icon: const Icon(Icons.image),
+                  label: const Text(
+                    'Add main image',
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.blue,
+                    ),
+                  ),
                 ),
               ),
-            ),
-            minLines: 5,
-            maxLines: null,
-            style: TextStyle(
-              fontSize: 18,
-              color: Colors.black,
-            ),
-          ),
-          const SizedBox(height: 20),
-          const Center(
-            child: Text(
-              'Media',
-              style: TextStyle(
-                fontSize: 35,
-                fontWeight: FontWeight.bold,
+              const SizedBox(height: 20),
+              const Center(
+                child: TextField(
+                  decoration: InputDecoration(hintText: 'Case title'),
+                  maxLines: 1,
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.black,
+                  ),
+                ),
               ),
-            ),
+              const SizedBox(height: 20),
+              const Center(
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Short description',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(8),
+                      ),
+                    ),
+                  ),
+                  maxLines: 3,
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              const TextField(
+                decoration: InputDecoration(
+                  hintText: 'Detailed description',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(8),
+                    ),
+                  ),
+                ),
+                minLines: 10,
+                maxLines: null,
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.black,
+                ),
+              ),
+              const SizedBox(height: 20),
+              const Center(
+                child: Text(
+                  'Media',
+                  style: TextStyle(
+                    fontSize: 35,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Photos',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 20),
+              GridView.count(
+                shrinkWrap: true,
+                mainAxisSpacing: 4,
+                crossAxisSpacing: 4,
+                physics: const NeverScrollableScrollPhysics(),
+                crossAxisCount: 3,
+                children: images
+                    .map((image) => Image.asset(
+                          'assets/$image',
+                          height: 150,
+                          fit: BoxFit.cover,
+                        ))
+                    .toList(),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Videos',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 20),
+              GridView.count(
+                shrinkWrap: true,
+                mainAxisSpacing: 4,
+                crossAxisSpacing: 4,
+                physics: const NeverScrollableScrollPhysics(),
+                crossAxisCount: 3,
+                children: images
+                    .map((image) => Image.asset(
+                          'assets/$image',
+                          height: 150,
+                          fit: BoxFit.cover,
+                        ))
+                    .toList(),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Audio',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'External links',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
           ),
-          const SizedBox(height: 20),
-          const Text(
-            'Photos',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          const SizedBox(height: 20),
-          GridView.count(
-            shrinkWrap: true,
-            mainAxisSpacing: 4,
-            crossAxisSpacing: 4,
-            physics: const NeverScrollableScrollPhysics(),
-            crossAxisCount: 3,
-            children: images
-                .map((image) => Image.asset(
-                      'assets/$image',
-                      height: 150,
-                      fit: BoxFit.cover,
-                    ))
-                .toList(),
-          ),
-          const SizedBox(height: 20),
-          const Text(
-            'Videos',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          const SizedBox(height: 20),
-          GridView.count(
-            shrinkWrap: true,
-            mainAxisSpacing: 4,
-            crossAxisSpacing: 4,
-            physics: const NeverScrollableScrollPhysics(),
-            crossAxisCount: 3,
-            children: images
-                .map((image) => Image.asset(
-                      'assets/$image',
-                      height: 150,
-                      fit: BoxFit.cover,
-                    ))
-                .toList(),
-          ),
-          const SizedBox(height: 20),
-          const Text(
-            'Audio',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          const SizedBox(height: 20),
-          const Text(
-            'External links',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
